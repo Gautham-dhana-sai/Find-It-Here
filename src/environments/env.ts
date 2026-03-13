c2a328f2f50b96d849b0c54038a12ea1a3e337f5
Goutham Dhana Sai <gouthamdhanasai.p@inncircles.com>
2026-03-13 20:27:20 +0530

    server url updated

--- src/environments/env.ts
+++ src/environments/env.ts
@@ -1,7 +1,7 @@
 export const env = {
     secretKey: 'ABCD',
     
-    apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://find-it-here-server.vercel.app/',
     loginApiUrl: 'https://dhana-login-server.vercel.app',
     locationApiUrl: 'https://countriesnow.space/api/v0.1/countries'
 }
